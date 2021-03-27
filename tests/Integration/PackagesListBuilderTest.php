482d145bec3319e8c3ea42ee34361b963d923b02
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-03-27 15:10:54 +0300

    Merge PackagesListBuilder tests.

--- tests/Integration/PackagesListBuilderTest.php
+++ tests/Integration/PackagesListBuilderTest.php
@@ -153,4 +153,127 @@ final class PackagesListBuilderTest extends ComposerTest
             ],
         ]);
     }
+
+    public function testRequireDev(): void
+    {
+        $this->initComposer([
+            'require' => [
+                'yiisoft/config' => '*',
+                'test/d-dev-c' => '*',
+            ],
+            'require-dev' => [
+                'test/a' => '*',
+            ],
+        ]);
+
+        $this->assertMergePlan([
+            'params' => [
+                'test/a' => [
+                    'config/params.php',
+                ],
+            ],
+            'web' => [
+                'test/d-dev-c' => [
+                    'config/web.php',
+                ],
+                'test/a' => [
+                    'config/web.php',
+                ],
+            ],
+        ]);
+    }
+
+    public function testSourceDirectoryOption(): void
+    {
+        $this->initComposer([
+            'require' => [
+                'yiisoft/config' => '*',
+                'test/a' => '*',
+            ],
+            'extra' => [
+                'config-plugin-options' => [
+                    'source-directory' => 'app-configs',
+                ],
+                'config-plugin' => [
+                    'params' => [
+                        'params.php',
+                        '?params-local.php',
+                    ],
+                    'web' => 'web.php',
+                ],
+            ],
+        ]);
+
+        $this->assertMergePlan([
+            'params' => [
+                '/' => [
+                    'app-configs/params.php',
+                    '?app-configs/params-local.php',
+                ],
+                'test/a' => [
+                    'config/params.php',
+                ],
+            ],
+            'web' => [
+                '/' => [
+                    'app-configs/web.php',
+                ],
+                'test/a' => [
+                    'config/web.php',
+                ],
+            ],
+        ]);
+    }
+
+    public function testVariables(): void
+    {
+        $this->initComposer([
+            'require' => [
+                'yiisoft/config' => '*',
+                'test/a' => '*',
+            ],
+            'extra' => [
+                'config-plugin-options' => [
+                    'source-directory' => 'app-configs',
+                ],
+                'config-plugin' => [
+                    'params' => [
+                        'params.php',
+                        '?params-local.php',
+                    ],
+                    'common' => 'common.php',
+                    'web' => [
+                        '$common',
+                        'web.php',
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertMergePlan([
+            'common' => [
+                '/' => [
+                    'app-configs/common.php',
+                ],
+            ],
+            'params' => [
+                '/' => [
+                    'app-configs/params.php',
+                    '?app-configs/params-local.php',
+                ],
+                'test/a' => [
+                    'config/params.php',
+                ],
+            ],
+            'web' => [
+                '/' => [
+                    '$common',
+                    'app-configs/web.php',
+                ],
+                'test/a' => [
+                    'config/web.php',
+                ],
+            ],
+        ]);
+    }
 }
