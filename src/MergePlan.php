f58ea0ec6d4bde30e74a3d785bbf1e6d36485350
devanych <mail@devanych.ru>
2021-09-08 19:36:03 +0300

    Add MergePlan class

---
+++ src/MergePlan.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Config;
+
+/**
+ * @internal
+ */
+final class MergePlan
+{
+    /**
+     * @psalm-var array<string, array<string, array<string, string[]>>>
+     */
+    private array $mergePlan;
+
+    /**
+     * @psalm-param array<string, array<string, array<string, string[]>>> $mergePlan
+     */
+    public function __construct(array $mergePlan = [])
+    {
+        $this->mergePlan = $mergePlan;
+    }
+
+    /**
+     * Adds an item to the merge plan.
+     *
+     * @param string $file The config file.
+     * @param string $package The package name.
+     * @param string $group The group name.
+     * @param string $environment The environment name.
+     */
+    public function add(
+        string $file,
+        string $package,
+        string $group,
+        string $environment = Options::DEFAULT_ENVIRONMENT
+    ): void {
+        $this->mergePlan[$environment][$group][$package][] = $file;
+    }
+
+    /**
+     * Adds a multiple items to the merge plan.
+     *
+     * @param string[] $files The config files.
+     * @param string $package The package name.
+     * @param string $group The group name.
+     * @param string $environment The environment name.
+     */
+    public function addMultiple(
+        array $files,
+        string $package,
+        string $group,
+        string $environment = Options::DEFAULT_ENVIRONMENT
+    ): void {
+        $this->mergePlan[$environment][$group][$package] = $files;
+    }
+
+    /**
+     * Returns the merge plan group.
+     *
+     * @param string $group The group name.
+     * @param string $environment The environment name.
+     *
+     * @return array<string, string[]>
+     */
+    public function getGroup(string $group, string $environment = Options::DEFAULT_ENVIRONMENT): array
+    {
+        return $this->mergePlan[$environment][$group] ?? [];
+    }
+
+    /**
+     * Returns the merge plan as an array.
+     *
+     * @psalm-return array<string, array<string, array<string, string[]>>>
+     */
+    public function toArray(): array
+    {
+        return $this->mergePlan;
+    }
+
+    /**
+     * Checks whether the group exists in the merge plan.
+     *
+     * @param string $group The group name.
+     * @param string $environment The environment name.
+     *
+     * @return bool Whether the group exists in the merge plan.
+     */
+    public function hasGroup(string $group, string $environment = Options::DEFAULT_ENVIRONMENT): bool
+    {
+        return isset($this->mergePlan[$environment][$group]);
+    }
+
+    /**
+     * Checks whether the environment exists in the merge plan.
+     *
+     * @param string $environment The environment name.
+     *
+     * @return bool Whether the environment exists in the merge plan.
+     */
+    public function hasEnvironment(string $environment): bool
+    {
+        return isset($this->mergePlan[$environment]);
+    }
+}
