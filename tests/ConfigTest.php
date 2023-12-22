bd5edf0ca184c0d10551f3dc865ac32472289891
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-22 16:48:30 +0300

    18

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -13,27 +13,6 @@ use Yiisoft\Config\Options;
 
 final class ConfigTest extends TestCase
 {
-    public function testConfigWithCustomParams(): void
-    {
-        $config = new Config(
-            new ConfigPaths(__DIR__ . '/TestAsset/configs/custom-params', 'config'),
-            Options::DEFAULT_ENVIRONMENT,
-            [],
-            'custom-params'
-        );
-
-        $this->assertSame(
-            [
-                'a-web-key' => 'a-web-value',
-                'a-web-environment-override-key' => 'a-web-override-value',
-                'b-web-key' => 'b-web-value',
-                'b-web-environment-override-key' => 'b-web-override-value',
-                'root-web-key' => 42,
-            ],
-            $config->get('web')
-        );
-    }
-
     public function testRemoveFromVendor(): void
     {
         $config = new Config(
