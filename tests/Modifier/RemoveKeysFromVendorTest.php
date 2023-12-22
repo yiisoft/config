9326da153b478443ceaf035098be5676f3881257
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-12-22 17:03:15 +0300

    add tests

--- tests/Modifier/RemoveKeysFromVendorTest.php
+++ tests/Modifier/RemoveKeysFromVendorTest.php
@@ -10,6 +10,14 @@ use Yiisoft\Config\Modifier\RemoveFromVendor;
 
 final class RemoveKeysFromVendorTest extends TestCase
 {
+    public function testBase()
+    {
+        $modifier = RemoveFromVendor::keys(['app'])->package('test/a', 'test/b');
+
+        $this->assertSame([['app']], $modifier->getKeys());
+        $this->assertSame([['test/a', 'test/b']], $modifier->getPackages());
+    }
+
     public function testInvalidPackage(): void
     {
         $this->expectException(InvalidArgumentException::class);
